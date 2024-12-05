9b7c375b7cd665371d760f7bdaf01a1329bd95ad
Lokman Hossen <lokmanhossen230@gmail.com>
2024-12-05 22:50:20 +0600

    Add SideLogo

---
+++ lib/widgets/side_logo.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:my_portfolio/cnstants/colors.dart';
+
+class SideLogo extends StatelessWidget {
+  const SideLogo({super.key, this.onTap});
+
+  final VoidCallback? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: const Text(
+        'LK',
+        style: TextStyle(
+          fontSize: 22,
+          fontWeight: FontWeight.bold,
+          decoration: TextDecoration.underline,
+          color: CustomColor.yellowSecondary,
+        ),
+      ),
+    );
+  }
+}
