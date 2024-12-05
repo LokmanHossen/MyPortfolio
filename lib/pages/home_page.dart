a0fb10193ff96dd22686f6cb92c67870c842aa75
Lokman Hossen <lokmanhossen230@gmail.com>
2024-12-05 15:42:41 +0600

    NavigationItem

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:my_portfolio/cnstants/colors.dart';
+import 'package:my_portfolio/cnstants/nav_items.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,43 +13,84 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: CustomColor.scaffoldBg,
         body: ListView(
-      scrollDirection: Axis.vertical,
-      children: [
-        // MAIN
-        Container(
-          height: 500,
-          width: double.infinity,
-          color: Colors.blue,
-        ),
-        //SKill
-        Container(
-          height: 500,
-          width: double.infinity,
-          color: Colors.amber,
-        ),
+          scrollDirection: Axis.vertical,
+          children: [
+            // MAIN
+            Container(
+              height: 60.0,
+              margin: const EdgeInsets.symmetric(
+                vertical: 10.0,
+                horizontal: 20.0,
+              ),
+              width: double.maxFinite,
+              decoration: BoxDecoration(
+                  gradient: const LinearGradient(
+                    colors: [
+                      Colors.transparent,
+                      CustomColor.bgLight1,
+                    ],
+                  ),
+                  borderRadius: BorderRadius.circular(100)),
+              child: Row(
+                children: [
+                  const Text(
+                    'LK',
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                      decoration: TextDecoration.underline,
+                      color: CustomColor.yellowSecondary,
+                    ),
+                  ),
+                  const Spacer(),
+                  for (int i = 0; i < navTitles.length; i++)
+                    Padding(
+                      padding: const EdgeInsets.only(right: 10.0),
+                      child: TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          navTitles[i],
+                          style: const TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                            color: CustomColor.whitePrimary,
+                          ),
+                        ),
+                      ),
+                    ),
+                ],
+              ),
+            ),
+            //SKill
+            Container(
+              height: 500,
+              width: double.infinity,
+              color: Colors.amber,
+            ),
 
-        //Projec
-        Container(
-          height: 500,
-          width: double.infinity,
-          color: Colors.black,
-        ),
+            //Projec
+            Container(
+              height: 500,
+              width: double.infinity,
+              color: Colors.black,
+            ),
 
-        //contact
-        Container(
-          height: 500,
-          width: double.infinity,
-          color: Colors.blue,
-        ),
+            //contact
+            Container(
+              height: 500,
+              width: double.infinity,
+              color: Colors.blue,
+            ),
 
-        //Footer
-        Container(
-          height: 500,
-          width: double.infinity,
-          color: Colors.green,
-        )
-      ],
-    ));
+            //Footer
+            Container(
+              height: 500,
+              width: double.infinity,
+              color: Colors.green,
+            )
+          ],
+        ));
   }
 }
