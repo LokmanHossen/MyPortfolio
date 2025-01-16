5810f1646f96fde1bd4b5baaef13f11bc47596db
Lokman Hossen <lokmanhossen230@gmail.com>
2025-01-16 23:09:27 +0600

    HomePage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:my_portfolio/cnstants/colors.dart';
-import 'package:my_portfolio/cnstants/nav_items.dart';
+
+import '../widgets/header_mobile.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -18,51 +19,12 @@ class _HomePageState extends State<HomePage> {
           scrollDirection: Axis.vertical,
           children: [
             // MAIN
-            Container(
-              height: 60.0,
-              margin: const EdgeInsets.symmetric(
-                vertical: 10.0,
-                horizontal: 20.0,
-              ),
-              width: double.maxFinite,
-              decoration: BoxDecoration(
-                  gradient: const LinearGradient(
-                    colors: [
-                      Colors.transparent,
-                      CustomColor.bgLight1,
-                    ],
-                  ),
-                  borderRadius: BorderRadius.circular(100)),
-              child: Row(
-                children: [
-                  const Text(
-                    'LK',
-                    style: TextStyle(
-                      fontSize: 22,
-                      fontWeight: FontWeight.bold,
-                      decoration: TextDecoration.underline,
-                      color: CustomColor.yellowSecondary,
-                    ),
-                  ),
-                  const Spacer(),
-                  for (int i = 0; i < navTitles.length; i++)
-                    Padding(
-                      padding: const EdgeInsets.only(right: 10.0),
-                      child: TextButton(
-                        onPressed: () {},
-                        child: Text(
-                          navTitles[i],
-                          style: const TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w500,
-                            color: CustomColor.whitePrimary,
-                          ),
-                        ),
-                      ),
-                    ),
-                ],
-              ),
+            // const HeaderDesktop(),
+            HeaderMobile(
+              onLogoTap: () {},
+              onMenuTap: () {},
             ),
+
             //SKill
             Container(
               height: 500,
