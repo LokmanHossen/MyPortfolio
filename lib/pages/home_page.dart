4e418a50761baee9b6ae183dafe89518e3a8a839
Lokman Hossen <lokmanhossen230@gmail.com>
2025-01-17 08:00:29 +0600

    AddEndDrawer

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:my_portfolio/cnstants/colors.dart';
 
+import '../cnstants/nav_items.dart';
 import '../widgets/header_mobile.dart';
 
 class HomePage extends StatefulWidget {
@@ -11,48 +12,72 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final scaffoldKey = GlobalKey<ScaffoldState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: scaffoldKey,
+      backgroundColor: CustomColor.scaffoldBg,
+      endDrawer: Drawer(
         backgroundColor: CustomColor.scaffoldBg,
-        body: ListView(
-          scrollDirection: Axis.vertical,
+        child: ListView(
           children: [
-            // MAIN
-            // const HeaderDesktop(),
-            HeaderMobile(
-              onLogoTap: () {},
-              onMenuTap: () {},
-            ),
-
-            //SKill
-            Container(
-              height: 500,
-              width: double.infinity,
-              color: Colors.amber,
-            ),
-
-            //Projec
-            Container(
-              height: 500,
-              width: double.infinity,
-              color: Colors.black,
-            ),
-
-            //contact
-            Container(
-              height: 500,
-              width: double.infinity,
-              color: Colors.blue,
-            ),
-
-            //Footer
-            Container(
-              height: 500,
-              width: double.infinity,
-              color: Colors.green,
-            )
+            for (int i = 0; i < navTitles.length; i++)
+              ListTile(
+                contentPadding: const EdgeInsets.symmetric(horizontal: 30),
+                titleTextStyle: const TextStyle(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 20,
+                  color: CustomColor.whitePrimary,
+                ),
+                leading: Icon(navIcon[i]),
+                title: Text(navTitles[i]),
+                onTap: () {},
+              ),
           ],
-        ));
+        ),
+      ),
+      body: ListView(
+        scrollDirection: Axis.vertical,
+        children: [
+          // MAIN
+          // const HeaderDesktop(),
+          HeaderMobile(
+            onLogoTap: () {},
+            onMenuTap: () {
+              scaffoldKey.currentState!.openEndDrawer();
+            },
+          ),
+
+          //SKill
+          Container(
+            height: 500,
+            width: double.infinity,
+            color: Colors.amber,
+          ),
+
+          //Projec
+          Container(
+            height: 500,
+            width: double.infinity,
+            color: Colors.black,
+          ),
+
+          //contact
+          Container(
+            height: 500,
+            width: double.infinity,
+            color: Colors.blue,
+          ),
+
+          //Footer
+          Container(
+            height: 500,
+            width: double.infinity,
+            color: Colors.green,
+          )
+        ],
+      ),
+    );
   }
 }
